e68c10bb4193720d4b1a8044b2b3aa6c1a0445cd
Ross Acheson <rossacheson@users.noreply.github.com>
2020-05-07 14:07:12 -0400

    Add column with named foreign key
    Add example for adding non-nullable column with named foreign key

--- sql-server/AddNonNullableColumn.sql
+++ sql-server/AddNonNullableColumn.sql
@@ -17,3 +17,18 @@ IF NOT EXISTS(
 
 END
 GO
+
+-- Add ColumnName column to TableName with named foreign key
+IF NOT EXISTS(
+        SELECT 1
+        FROM [INFORMATION_SCHEMA].[COLUMNS]
+        WHERE [TABLE_SCHEMA] = 'dbo'
+            AND [TABLE_NAME] = 'TableName'
+            AND [COLUMN_NAME] = 'ColumnName'
+    )
+    BEGIN
+        ALTER TABLE dbo.[TableName] ADD ColumnName BIGINT CONSTRAINT FK_TableName_OtherTable FOREIGN KEY REFERENCES dbo.OtherTable(ID) NULL
+        EXEC ('UPDATE dbo.[TableName] SET ColumnName = (SELECT ID FROM dbo.OtherTable WHERE 1 = 1)')
+        ALTER TABLE dbo.[TableName] ALTER COLUMN ColumnName BIGINT NOT NULL
+    END
+GO
